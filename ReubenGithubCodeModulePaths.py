7fd331345b4216b58c843de41314e4ef6290188c
Reuben Brewer <reuben.brewer@gmail.com>
2026-01-09 18:10:14 -0800

    Delete ReubenGithubCodeModulePaths.py

--- ReubenGithubCodeModulePaths.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pathlib import Path
-
-GDRIVE_ROOT = Path(r"D:\Laza\Robotics - Documents\Software\Code_ReubenGithub")
-
-def Enable():
-    PathToAdd = str(GDRIVE_ROOT)
-    if PathToAdd not in sys.path:
-        sys.path.insert(0, PathToAdd)
